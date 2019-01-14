bb06d27e08a1de0f85a0ba1d8760fa68f3b5aace
Michell <admin@TA-829-INS>
2019-01-14 17:26:25 +0100

    oprydn

--- Variabler1/Program.cs
+++ Variabler1/Program.cs
@@ -10,6 +10,109 @@ namespace Variabler1
     {
         static void Main(string[] args)
         {
+            {
+                int a = 10;
+
+                {
+                    a = 20;
+                    int c = 20;
+                        
+                        
+                        
+                }
+
+                {
+                    a = 30;
+                    
+                }
+
+                
+            }
+            {
+                int w = 200;
+                for (int i = 0; i < 10; i++)
+                {
+                    i = 400;
+                    w++;
+
+                }
+                Console.WriteLine(w);
+
+
+                {
+                    int g = 3;
+
+                }
+
+                int antalMånederPrÅr = 12;
+                if (antalMånederPrÅr < 10)
+                {
+
+                }
+                else
+                {
+
+                }
+
+                //int a;
+                //a = 0;
+
+                int b = new int();
+                //int b;
+
+
+                //int u = new int();
+                int u = 0;
+                u = 100;
+                Console.WriteLine(u.ToString());
+
+                Console.WriteLine(int.MinValue);
+
+                DateTime d = new DateTime(2018, 1, 1);
+                // instans medlem
+
+                Console.WriteLine(d.ToLongDateString());
+
+                // statisk medlemmer
+                Console.WriteLine(DateTime.Now);
+                Console.WriteLine(DateTime.IsLeapYear(2018));
+
+
+                int s = 338585858;
+
+
+                double d1 = 23890430.345;
+                decimal d2 = 9489348753945.345M;
+                float d3 = 3434.3F;
+
+
+                // int, double
+                int i1 = 3490505;
+                double i2 = 34095345.34;
+
+
+                int yy = 1;
+                yy = yy + 1;
+                yy++;
+
+                yy += 10;
+                yy = yy + 10;
+
+            }
+
+            {
+                checked
+                {
+                    byte l = 255;
+                    l += 20;
+                    Console.WriteLine(l);
+                }
+            }
+
+
+
+
+
         }
     }
 }
